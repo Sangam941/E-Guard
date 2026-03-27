5e58d9ba0bcec66ebf5eb51d91a3c6ab253c73b3
Bibekadhikari77 <adhikaribibek84@gmail.com>
2026-03-27 21:30:03 +0545

    push backend

--- server/services/gemini.services.js
+++ server/services/gemini.services.js
@@ -14,7 +14,7 @@ export async function generateContent(prompt) {
                 contents: [
                     {
                         parts: [{ text: prompt }]
-                    }
+                    } 
                 ]
             })
         });
