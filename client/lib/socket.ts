5ac263ba10b3d90286c9ded49f23d782a10ed1bd
Sangam Singh Dhami <sangamdhami663@gmail.com>
2026-03-28 21:55:04 +0545

    for deployment

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const URL = process.env.NEXT_PUBLIC_SOCKET || 'http://localhost:5000';
 
 export const socket = io(URL, {
   autoConnect: false, // Connect manually when user is authenticated
